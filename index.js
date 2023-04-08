fc88af4db9c27ec68484e9fed6b6cd421e2a6256
titan-rocky <rockalbert466@gmail.com>
2023-04-08 14:50:00 +0530

    node

---
+++ index.js
@@ -0,0 +1,34 @@
+const host = 'localhost';
+const port = 8000;
+const http = require("http");
+var express=require("express");
+
+var app=express();
+app.use(express.static('static'));
+app.use('/css',express.static('static'));
+app.use('/tsx',express.static('static'));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+app.get('/', function (request, response) {
+        response.sendFile(__dirname+"/index.html")
+});
+
+app.post('/login' , function(request , response){
+  console.log(request.body)
+  response.json(request.body)
+})
+/*
+
+const requestListener = function(req,res){
+	res.writeHead(200);
+	const fs=require("fs").promises;
+	fs.readFile("D:/Github/clicking_contest/static/index.html");
+	return;
+};
+const server = http.createServer(requestListener);
+*/
+
+app.listen(port, host, () => {
+    console.log(`Server is running on http://${host}:${port}`);
+});
---
+++ static/oma.tsx
@@ -0,0 +1,4 @@
+import React from 'react'
+import { react } from 'react-dom'
+console.log("Hello World")
+
